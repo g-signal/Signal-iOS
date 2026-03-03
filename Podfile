2151cbbcf811eec6adaf7a48a6aa3f251d02be25
yangweiguang <yangwg0809@163.com>
2026-03-03 16:29:47 +0800

    change enclave id

--- Podfile
+++ Podfile
@@ -13,7 +13,7 @@ pod 'SwiftProtobuf', "1.30.0"
 
 #ENV['LIBSIGNAL_FFI_PREBUILD_CHECKSUM'] = 'ebaf33d71937087c5a5d6eb3ce52d67152fb25eb677b5bb6d29897a040f270f7'
 #pod 'LibSignalClient', git: 'https://github.com/signalapp/libsignal.git', tag: 'v0.76.7', testspecs: ["Tests"]
-ENV['LIBSIGNAL_FFI_PREBUILD_CHECKSUM'] = 'aeb3e74459c5dce072ba2741661d5fd59773246e1bd73311413f6f85f688943d'
+ENV['LIBSIGNAL_FFI_PREBUILD_CHECKSUM'] = '72391b69443226d122c501696f165a33cc5256c4a461f51ecdf4b04f8ae8ea26'
 pod 'LibSignalClient', git: 'https://github.com/g-signal/libsignal.git', tag: 'v0.76.7-BA', testspecs: ["Tests"]
 #pod 'LibSignalClient', path: '/Volumes/STAR/Document/glx/signal/libsignal', testspecs: ["Tests"]
 
--- SignalServiceKit/TSConstants.swift
+++ SignalServiceKit/TSConstants.swift
@@ -182,15 +182,14 @@ public class TSConstantsProduction: TSConstantsProtocol {
     public let storageServiceCensorshipPrefix = "storage"
     public let svr2CensorshipPrefix = "svr2"
 
-    public let svr2Enclave = MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f")
+    public let svr2Enclave = MrEnclave("97858810df5aaecaa10048fc56931badce985e5b260ca623993a363b8f5d760e")
 
     // An array of previously used enclaves that we should try and restore
     // key material from during registration. These must be ordered from
     // newest to oldest, so we check the latest enclaves for backups before
     // checking earlier enclaves.
     public let svr2PreviousEnclaves: [MrEnclave] = [
-        MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f"),
-        MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f"),
+        MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f")
     ]
 
     public let applicationGroup = "group." + Bundle.main.bundleIdPrefix + ".group"
@@ -238,15 +237,14 @@ public class TSConstantsStaging: TSConstantsProtocol {
     public let storageServiceCensorshipPrefix = "storage-staging"
     public let svr2CensorshipPrefix = "svr2-staging"
 
-    public let svr2Enclave = MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f")
+    public let svr2Enclave = MrEnclave("97858810df5aaecaa10048fc56931badce985e5b260ca623993a363b8f5d760e")
 
     // An array of previously used enclaves that we should try and restore
     // key material from during registration. These must be ordered from
     // newest to oldest, so we check the latest enclaves for backups before
     // checking earlier enclaves.
     public let svr2PreviousEnclaves: [MrEnclave] = [
-        MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f"),
-        MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f"),
+        MrEnclave("b49a2d7aa6a92623713541be3342cc2432cbb4052a9ab83b50aef3375651e68f")
     ]
 
     public let applicationGroup = "group." + Bundle.main.bundleIdPrefix + ".group.staging"
