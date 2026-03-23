3227bc8e793851bba6c0bd0e8c1a9ce936454443
yangweiguang <yangwg0809@163.com>
2026-03-23 16:54:29 +0800

    rm group conversation id

--- Signal/src/ViewControllers/ThreadSettings/ConversationSettingsViewController+Contents.swift
+++ Signal/src/ViewControllers/ThreadSettings/ConversationSettingsViewController+Contents.swift
@@ -839,17 +839,6 @@ extension ConversationSettingsViewController {
             })
         )
 
-        // Add Group ID display item
-        let groupIdHex = groupModelV2.groupId.hexadecimalString
-        section.add(OWSTableItem.copyableItem(
-            label: OWSLocalizedString(
-                "CONVERSATION_SETTINGS_GROUP_ID",
-                comment: "Label for 'group ID' item in conversation settings view."
-            ),
-            value: groupIdHex,
-            accessibilityIdentifier: "group_id_item"
-        ))
-
         if canEditPermissions {
             let itemTitle = OWSLocalizedString(
                 "CONVERSATION_SETTINGS_PERMISSIONS",
